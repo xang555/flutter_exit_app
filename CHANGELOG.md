aeac2b5c83185f680a804902d7d71a9cd9bb80f7
xang Ultimate <xang.ultimate@gmail.com>
2022-03-01 10:19:43 +0700

    #add: exit android app when 200 msec #update package version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,10 @@
 # 1.0.0
 
-## CHANGELOGS
-
 * Implement native android exit app
 * Implement native ios exit app
 * Add License
+
+# 1.0.3
+
+* fix docs
+* android exit when 2000 msec
--- android/src/main/java/com/laoitdev/lib/exit/app/flutter_exit_app/FlutterExitAppPlugin.java
+++ android/src/main/java/com/laoitdev/lib/exit/app/flutter_exit_app/FlutterExitAppPlugin.java
@@ -13,6 +13,7 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
+import android.os.Handler;
 
 /** FlutterExitAppPlugin */
 public class FlutterExitAppPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
@@ -22,6 +23,9 @@ public class FlutterExitAppPlugin implements FlutterPlugin, MethodCallHandler, A
   /// when the Flutter Engine is detached from the Activity
   private MethodChannel channel;
   private Activity activity;
+
+  // handler
+  final Handler handler = new Handler();
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
@@ -33,13 +37,25 @@ public class FlutterExitAppPlugin implements FlutterPlugin, MethodCallHandler, A
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
     if (call.method.equals("getPlatformVersion")) {
       result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else if (call.method.equals(ChannelName.exitapp)){
+    } else if (call.method.equals(ChannelName.exitapp)){  
       if(Build.VERSION.SDK_INT>=16 && Build.VERSION.SDK_INT<21){
         activity.finishAffinity();
+        handler.postDelayed(new Runnable() {
+          @Override
+          public void run() {
+            System.exit(0);
+          }
+        }, 2000);
         result.success("Done");
         return;
       } else if(Build.VERSION.SDK_INT>=21){
         activity.finishAndRemoveTask();
+        handler.postDelayed(new Runnable() {
+          @Override
+          public void run() {
+            System.exit(0);
+          }
+        }, 2000);
         result.success("Done");
         return;
       }
